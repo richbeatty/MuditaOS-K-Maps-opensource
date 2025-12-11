97cee4df7114c26397597a8ee272911b9144fd22
richbeatty <99283667+richbeatty@users.noreply.github.com>
2025-12-11 11:37:09 -0500

    Fix Gradle build configuration for local builds

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,32 +1,9 @@
 pluginManagement {
-    val properties = java.util.Properties()
-    file("local.properties").takeIf { it.isFile }?.inputStream()?.use {
-        properties.load(java.io.InputStreamReader(it, Charsets.UTF_8))
-    }
-
-    extra["muditaUsername"] = properties.getProperty("mudita_repo_username")
-        ?: System.getenv("ARTIFACTORY_USERNAME")
-    extra["muditaPassword"] = properties.getProperty("mudita_repo_password")
-        ?: System.getenv("ARTIFACTORY_PASSWORD")
-    extra["muditaRepoUrl"] = properties.getProperty("mudita_nexus_repo_url")
-
     repositories {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven {
-            url = uri(extra["muditaRepoUrl"].toString())
-            credentials {
-                username = extra["muditaUsername"].toString()
-                password = extra["muditaPassword"].toString()
-            }
-        }
     }
-}
-
-plugins {
-    val sentryPlugin = "0.0.43"
-    id("com.mudita.sentry.plugins.release") version sentryPlugin apply false
 }
 
 include(":app")
